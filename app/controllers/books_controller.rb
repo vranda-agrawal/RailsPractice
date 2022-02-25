c0b27b53546db71722c053e7c70ee7c9353d8cee
Vranda Agrawal <vranda.a@simformsolutions.com>
2022-02-25 17:59:06 +0530

    layout-practice

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  layout "book" ,except: [:index]
   def index
     puts "-----------------------inside index-------------------"
     render variants: determine_variant
@@ -11,7 +12,32 @@ class BooksController < ApplicationController
   end
 
   def file
-    render file: "#{Rails.root}/public/404.html",layout: false
+    render file: "#{Rails.root}/public/404.html",layout: true
+  end
+
+  #Avoiding double render error
+  def show
+    @book = {'key1':'value1','key2':'value2'}
+    if !@book.empty?
+      render action: "show" and return
+    end
+    render action: "default"
+  end
+
+  #here writing render means it will only render html page and will not make any new request
+  # def edit
+  #   @book = {'key1':'value1','key2':'value2'}
+  #   if !@book.empty?
+  #     render action: "show"
+  #   end
+  # end
+
+  #here writing redirect means it will make a new request
+  def edit
+    @book = {'key1':'value1','key2':'value2'}
+    if !@book.empty?
+      redirect_to action: "index"
+    end
   end
   private
 
